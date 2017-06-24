c352ce80fdf85d3e12fa4b9ea1e896a6ae0cc352
jairsaidds <jairsaidds@gmail.com>
2017-06-24 10:01:44 -0500

    Add problems and reduce lines in implementation of UFDS

--- Union Find/ufds.cpp
+++ Union Find/ufds.cpp
@@ -4,15 +4,11 @@ using namespace std;
 ll p[10000], r[10000], N, Q, x, y, h[10000];
 char o;
 void makeSet(ll x){
-    r[x] = 0;
-    h[x] = 1;
-    p[x] = x;
+    r[x] = 0, h[x] = 1, p[x] = x;
     return;
 }
 ll find(ll x){
-	if(x == p[x])
-		return x;
-	return p[x] = find(p[x]);
+	return x == p[x] ? x : p[x] = find(p[x]);
 }
 ll sameSet(ll x, ll y){
 	return find(x) == find(y);
@@ -24,16 +20,11 @@ void join(ll x, ll y){
 	if(!sameSet(x,y)){
 		ll dx = find(x);
 		ll dy = find(y);
-		if(r[dx] > r[dy]){
-			p[dy] = dx;
-			h[dx] += h[dy];
-		}
-		else{
-			p[dx] = dy;
-			h[dy] += h[dx];
-			if(r[dx] == r[dy])
-				r[dy]++;
-		}
+        if(r[dx] > r[dy])
+            swap(dx,dy);
+        p[dx] = dy;
+        h[dy] += h[dx];
+        if(r[dx] == r[dy]) ++r[dy];
 		N--;
 	}
 }
