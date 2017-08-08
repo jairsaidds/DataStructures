09f69c82e14385c0c18febfc63c15d4063edbba3
Said Reyes <jairsaidds@gmail.com>
2017-08-08 11:43:49 -0500

    Add Update Range function without Lazy Propagation - O(n) worst case

--- Segment Tree/STPointUpdates.cpp
+++ Segment Tree/STPointUpdates.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long int st[1010101], N, A[10101010], Q, T, x, y;
+long long int st[1010101], N, A[10101010], Q, T, x, y, v;
 void build(int p, int l, int r){
 	if(l == r){
 		st[p] = A[l];
@@ -35,6 +35,19 @@ void update(int p, int l, int r, int idx, int v){
 		st[p] = st[p << 1] + st[p << 1 | 1];		
 	}
 }
+void updateRange(int p, int l, int r, int i, int j, int v){
+	cout << "Visito" << endl;
+	if(r < i or j < l)
+		return;	
+	if(r == l){
+		st[p] = v;
+		return;
+	}
+	int m = l + (r - l) / 2;
+	updateRange(p << 1, l , m, i, j, v);
+	updateRange(p << 1 | 1, m + 1, r, i, j, v);
+	st[p] = st[p << 1] + st[p << 1 | 1];
+}
 int main(){	
 	cin >> N;
 	for(int i = 0; i < N; i++)
@@ -47,9 +60,14 @@ int main(){
 			cin >> x >> y;
 			cout << query(1, 0, N - 1, x, y) << endl;			
 		}else{
-			cin >> x >> y;
-			update(1, 0, N - 1, x, y);			
-		}
+			if(T == 2){
+				cin >> x >> y;
+				update(1, 0, N - 1, x, y);
+			}else{
+				cin >> x >> y >> v;
+				updateRange(1, 0, N - 1, x, y, v);
+			}	
+		}		
 	}
 	return 0;
 }
